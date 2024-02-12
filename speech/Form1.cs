874d8896450cd20642a152f0725c68eb470358f2
JameelAlredhi <jameel.alredhi@gmail.com>
2024-02-12 23:36:22 +0300

    frist code

---
+++ speech/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using SpeechLib;
+using speech;
+
+
+namespace speech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SpVoice read = new SpVoice();
+            read.Speak(textBox1.Text, SpeechVoiceSpeakFlags.SVSFDefault);
+        }
+    }
+}
